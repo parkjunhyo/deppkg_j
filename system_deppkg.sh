2f74458bd1a3741c3db75ca441478735d850070e
park junhyo <junhyo.luck@gmail.com>
2013-12-09 06:47:50 +0900

    Update system_deppkg.sh

--- system_deppkg.sh
+++ system_deppkg.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 ## The Packages for the System
-apt-get install -qqy --force-yes gcc uml-utilities libtool build-essential fakeroot graphviz debhelper autoconf automake dkms libssl-dev make po-debconf gettext file debianutils binutils util-linux module-init-tools
+apt-get install -qqy --force-yes gcc uml-utilities libtool build-essential fakeroot graphviz debhelper autoconf automake dkms libssl-dev make po-debconf gettext file debianutils binutils util-linux module-init-tools dbus
 apt-get install -qqy --force-yes linux-source kernel-package
 apt-get install -qqy --force-yes pkg-config linux-headers-`uname -r` linux-image-`uname -r`
 
